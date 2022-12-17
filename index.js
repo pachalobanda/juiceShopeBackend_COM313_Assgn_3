1587258be2b2efeb08121b876ccf8e561a556bd2
Keston P. Banda <bsc-inf-27-19@unima.ac.mw>
2022-12-16 16:30:59 -0800

    another change

--- index.js
+++ index.js
@@ -7,32 +7,26 @@ const { host, username, password, database } = process.env
 
 console.log(host, username, password, database)
 
-<<<<<<< HEAD
-try {
+
+
     const conn = mysql.createConnection({ host: 'localhost', user: 'root', password:'', database: 'juice store' })
-=======
-   const conn = mysql.createConnection({ host: 'localhost', user: 'root', password:'', database: 'database_name' })
->>>>>>> 6a45c8f7b120f6e9b54eedb5436dd0e2584545f1
+
    const tableSchema = `CREATE TABLE IF NOT EXISTS USERS (email varchar(100) ,password varchar(100))`
 
     conn.query(tableSchema)
 
-<<<<<<< HEAD
-    console.log(err)
-}
-=======
 
->>>>>>> 6a45c8f7b120f6e9b54eedb5436dd0e2584545f1
 const app = express()
+app.use(express.json())
 app.use(cors())
 
 app.post("/login", (req, res) => {
 
     try {
+        console.log(req.body)
         const { email, password } = req.body
         const sql = "INSERT INTO `users`( `email`, `password`) VALUES (?,?)"
         const id = conn.query(sql, [email, password])
-        console.log(id)
         res.sendStatus(200)
 
     } catch (error) {
